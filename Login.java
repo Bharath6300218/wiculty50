8143fab9879a9823bb7a4187886a0471a7467851
Nagesh <nageswara.rao@wiculty.com>
2025-07-07 15:35:54 +0530

    adbvbj

--- Login.java
+++ Login.java
@@ -2,12 +2,25 @@ public class Login {
 public static void main() {
     int i;
 
+<<<<<<< Updated upstream
     for(i=0;i<=30;i++){
+=======
+
+    for(i=0;i<=20;i++){
+
+  
+>>>>>>> Stashed changes
         System.out.println("Number: " + i);
     }
 }
 }
+<<<<<<< Updated upstream
 kfdkdf
 ngdsj
 ksjsfd
 vvjndfhdu
+=======
+tundhd
+gjdj
+djfjgn
+>>>>>>> Stashed changes
